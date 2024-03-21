c05b986da27ca865bab01679833573c43507ebc0
RJ <rj@macklem.ca>
2024-03-21 10:45:00 -0400

    Update ICE5_robert.py
    Added second login test and fixed CSS selector for registration test using chropath

--- ICE5_robert.py
+++ ICE5_robert.py
@@ -19,22 +19,22 @@ class TestMercuryTours():
   def teardown_method(self, method):
     self.driver.quit()
   
-  def test_login(self):
+  def test_login(self, username: str, password: str):
     #Get the site loaded
     self.driver.get("https://demo.guru99.com/test/newtours/")
     self.driver.set_window_size(1936, 1048)
 
     #Fill out login details (tutorial) + submit
     self.driver.find_element(By.NAME, "userName").click()
-    self.driver.find_element(By.NAME, "userName").send_keys("tutorial")
+    self.driver.find_element(By.NAME, "userName").send_keys(username)
     self.driver.find_element(By.NAME, "password").click()
-    self.driver.find_element(By.NAME, "password").send_keys("tutorial")
+    self.driver.find_element(By.NAME, "password").send_keys(password)
     self.driver.find_element(By.NAME, "submit").click()
     
     #Assert the login success based ont ext present
     assert self.driver.find_element(By.CSS_SELECTOR, "h3").text == "Login Successfully"
     
-  def test_registration(self):
+  def test_registration(self, username: str, password: str):
     #Get the site loaded
     self.driver.get("https://demo.guru99.com/test/newtours/")
     self.driver.set_window_size(1936, 1048)
@@ -61,7 +61,10 @@ class TestMercuryTours():
     self.driver.find_element(By.NAME, "submit").click()
 
     #Assert registration succesful based on text present
-    assert self.driver.find_element(By.CSS_SELECTOR, "p:nth-child(2) > font").text == "Thank you for registering. You may now sign-in using the user name and password you\\\'ve just entered."
+    assert "Thank you for registering." in self.driver.find_element(By.CSS_SELECTOR, "tbody:nth-child(1) tr:nth-child(3) td:nth-child(1) p:nth-child(2) > font:nth-child(1)").text  # ChroPath
+
+    #If assertion is successful, returnt he login credentials for further use
+    return username, password
 
 #Main  
 if __name__ == "__main__":
@@ -72,8 +75,9 @@ if __name__ == "__main__":
     test.setup_method(None)
 
     #Run Tests
-    test.test_login()
-    test.test_registration()
+    test.test_login("tutorial", "tutorial")  # Tutorial first
+    registration = test.test_registration("robert", "password1")  # Then build registration and test
+    test.test_login(registration[0], registration[1])  # Test new login credentials
 
     #Teardown
     test.teardown_method(None)
